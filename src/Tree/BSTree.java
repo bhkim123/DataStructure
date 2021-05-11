c8b9feef55f2c7d3fd853e92ccf144a325ce1aed
bhkim123 <82852354+bhkim123@users.noreply.github.com>
2021-05-11 18:06:05 -0400

    bs tree initial commit

--- src/Tree/BSTree.java
+++ src/Tree/BSTree.java
@@ -1,4 +1,84 @@
 package Tree;
 
-public class BSTree {
+import java.util.Collection;
+import java.util.Iterator;
+
+public class BSTree<E> {
+    Node<E> root;
+    int size;
+    int index;
+
+    public BSTree(){
+        this.root = null;
+        this.size = 0;
+    }
+
+    public boolean add(E e){
+
+    }
+
+    public boolean addAll(Collection<? extends E> c){
+
+    }
+
+    public boolean remove(Object o){
+
+    }
+
+    private boolean remove(Node<E> start, Object o){
+
+    }
+
+    private E getMin(Node<E> pointer){
+
+    }
+
+    public boolean contains(Object o){
+
+    }
+
+    private void inOrder(Node<E> pointer, E[] iterArr){
+
+    }
+
+    public Iterator<E> iterator() {
+
+    }
+
+    public int height(){
+
+    }
+
+    private int maxHeight(Node<E> root) {
+        if (root == null) {
+            return -1;
+        } else {
+            int leftMax = maxHeight(root.left);
+            int rightMax = maxHeight(root.right);
+
+            if (leftMax > rightMax) {
+                return leftMax + 1;
+            } else {
+                return rightMax + 1;
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static class Node<E>{
+        E value;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+
+        Node(E value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+        }
+    }
 }
